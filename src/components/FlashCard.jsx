9fe608f16b3d16473c170dc7d277243cd25ec176
Gregório Neto <igregoriodoneto@gmail.com>
2023-01-09 21:25:17 -0300

    FlashCard

---
+++ src/components/FlashCard.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+export default function FlashCard({
+  title = 'Título do card',
+  description = 'Descrição do card',
+  showFlashCardTitle = true,
+}) {
+  const [showTitle, setShowTitle] = useState(showFlashCardTitle);
+
+  useEffect(() => {
+    setShowTitle(showFlashCardTitle);
+  }, [showFlashCardTitle]);
+
+  function handleCardClick() {
+    setShowTitle(current => !current);
+  }
+
+  const fontSizeClassName = showTitle ? 'text-xl' : 'text-sm';
+  
+  return(
+    <div
+      className={`
+        shadow-lg p-4 m-2 w-80 h-48 cursor-pointer
+        flex flex-row items-center justify-center
+        font-semibold ${fontSizeClassName}
+      `}
+      style={{ fontFamily: "'JetBrains Mono', monospace" }}
+      onClick={handleCardClick}
+    >
+      { showTitle ? title : description }
+    </div>
+  );
+}
