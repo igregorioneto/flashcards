d9a24e91a58b8837ede4d82d424a9b97548154c0
Gregório Neto <igregoriodoneto@gmail.com>
2023-01-09 21:48:27 -0300

    Construindo Page do FlashCardsPage

--- src/pages/FlashCardPage.jsx
+++ src/pages/FlashCardPage.jsx
@@ -1,5 +1,76 @@
+import { useState } from "react";
+import { allFlashCards } from "../data/allFlashCards";
+import { shuffleArray } from "../helpers/arrayHelpers";
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Button from "../components/Button";
+import RadioButton from "../components/RadioButton";
+import FlashCards from "../components/FlashCards";
+import FlashCard from "../components/FlashCard";
+
 export default function FlashCardPage() {
+  const [allCards, setAllCards] = useState(allFlashCards);
+  const [showTitle, setShowTitle] = useState(false);
+
+  function handleButtonClick() {
+    const shuffedCards = shuffleArray(allCards);
+
+    setAllCards(shuffedCards);
+  }
+
+  function handleRadioShowDescription() {
+    setShowTitle(false);
+  }
+
+  function handleRadioShowTitle() {
+    setShowTitle(true);
+  }
+
   return(
-    <>Hello World!</>
+    <>
+      <Header>Flashcards</Header>
+      <Main>
+        <div className="text-center mb-4">
+          <Button
+            onButtonClick={handleButtonClick}
+          >
+            Embaralhar cards
+          </Button>
+        </div>
+
+        <div className="flex flex-row items-center justify-center space-x-4 m-4">
+          <RadioButton
+            id="radioButtonShowTitle"
+            name="showInfo"
+            buttonChecked={showTitle}
+            onButtonClick={handleRadioShowTitle}
+          >
+            Mostrar título
+          </RadioButton>
+
+          <RadioButton
+            id="radioButtonShowTitle"
+            name="showInfo"
+            buttonChecked={!showTitle}
+            onButtonClick={handleRadioShowDescription}
+          >
+            Mostrar descrição
+          </RadioButton>
+        </div>
+
+        <FlashCards>
+          {allCards.map(({id, title, description}) => {
+            return(
+              <FlashCard 
+                key={id}
+                title={title}
+                description={description}
+                showFlashCardTitle={showTitle}
+              />
+            );
+          })}
+        </FlashCards>
+      </Main>
+    </>
   );
 }
